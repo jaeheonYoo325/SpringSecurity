b15c26118d20a44d27c5f5f1fc732b3007655f7b
jaeheonYoo325 <yoojh0325@gmail.com>
2025-03-31 15:03:19 +0900

    커스텀 로그인 설정.

--- src/main/java/com/springsecurity/edu/SpringSecurity/controller/LoginController.java
+++ src/main/java/com/springsecurity/edu/SpringSecurity/controller/LoginController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class LoginController {
 
-    @GetMapping("/")
-    public String main() {
-        return "main";
+    @GetMapping("/login")
+    public String login() {
+
+        return "login";
     }
+
 }
