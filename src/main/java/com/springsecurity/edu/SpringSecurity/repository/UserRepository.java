14e0952f6b5c6472fbcab283d0512d87ef143638
jaeheonYoo325 <yoojh0325@gmail.com>
2025-03-31 16:53:47 +0900

    회원 중복 검증 로직 추가.

--- src/main/java/com/springsecurity/edu/SpringSecurity/repository/UserRepository.java
+++ src/main/java/com/springsecurity/edu/SpringSecurity/repository/UserRepository.java
@@ -5,4 +5,6 @@ import com.springsecurity.edu.SpringSecurity.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    boolean existsByUsername(String username);
 }
