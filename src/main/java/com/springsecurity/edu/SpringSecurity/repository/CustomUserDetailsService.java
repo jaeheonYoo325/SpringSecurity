bdee3ab5791e8e0c69c1f75f59196fd4d6cbd1b2
jaeheonYoo325 <yoojh0325@gmail.com>
2025-03-31 17:15:45 +0900

    DB기반 로그인 검증 로직 추가

---
+++ src/main/java/com/springsecurity/edu/SpringSecurity/repository/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.springsecurity.edu.SpringSecurity.repository;
+
+import com.springsecurity.edu.SpringSecurity.dto.CustomUserDetails;
+import com.springsecurity.edu.SpringSecurity.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
--- src/main/java/com/springsecurity/edu/SpringSecurity/repository/UserRepository.java
+++ src/main/java/com/springsecurity/edu/SpringSecurity/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     boolean existsByUsername(String username);
+
+    UserEntity findByUsername(String username);
 }
